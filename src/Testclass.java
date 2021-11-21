6c3d19a3aacb1eedb9a64a7b9dd3fc71f1f83dd8
senthilnathanramu161187 <senthilnathanramu161187@gmail.com>
2021-11-21 23:52:44 +0530

    Senthil's update

--- src/Testclass.java
+++ src/Testclass.java
@@ -4,5 +4,6 @@ public class Testclass {
 	public void newMethod() {
 		System.out.println("new by Ramya");
 		System.out.println("new by Senthil");
+		System.out.println("new by Senthilnathan ramu");
 	}
 }
